8e5963baced212943e880ca44bb868e5c49e89a4
Joan Disho <joan.disho@quickbirdstudios.com>
2017-10-09 13:35:24 +0200

    develop: create RealmProvider protocol.

---
+++ quick2doo/quick2doo/Utils/RealmProvider.swift
@@ -0,0 +1,42 @@
+//
+//  RealmProvider.swift
+//  quick2doo
+//
+//  Created by Joan Disho on 09.10.17.
+//  Copyright © 2017 Joan Disho. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+protocol RealmProvider {
+    var defaultRealm: Realm { get }
+    func performRealmOperation(_ operation: String, action: (Realm) -> Void) -> Bool 
+}
+
+extension RealmProvider {
+    
+    @discardableResult func performRealmOperation(_ operation: String, action: (Realm) -> Void) -> Bool {
+        do {
+            let realm = defaultRealm
+            try realm.write {
+                action(realm)
+            }
+            return true
+        } catch let err {
+            print("Failed \(operation) realm with error: \(err)")
+            return false
+        }
+    }
+    
+}
+
+class LocalRealmProvider: RealmProvider {
+    
+    var defaultRealm: Realm {
+        let configuration = Realm.Configuration(fileURL: nil, inMemoryIdentifier: nil, syncConfiguration: nil, encryptionKey: nil, readOnly: false, schemaVersion: 0, migrationBlock: nil, deleteRealmIfMigrationNeeded: true, shouldCompactOnLaunch: nil, objectTypes: nil)
+        
+        return try! Realm(configuration: configuration)
+    }
+    
+}
